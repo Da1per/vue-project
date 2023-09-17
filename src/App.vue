8ebc2a2b1046181a954e0efbfbebfcfbe7fb6556
dalper <dalper>
2023-09-17 12:23:41 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -24,12 +24,12 @@ export default {
     <div>
         <h1>Task</h1>
         <div class="block_task">
-            <div v-if="this.outText" class="block_task_begin">
+            <div  v-if="this.outText" class="block_task_begin">
                 {{this.outText}}
-                <button v-on:click="close">Close</button>
+                <button class="block_task_begin_but" v-on:click="close">Delete</button>
             </div>
             <textarea class="block_task_text" v-model='inText' placeholder="task...." />
-            <input class="block_task_but" type="button" v-on:click='add' value="Add Task">
+            <input :disabled="!this.inText" class="block_task_but" type="button" v-on:click='add' value="Add Task">
         </div>
         
     </div>
@@ -39,11 +39,32 @@ export default {
 
 <style scoped>
     .block_task_begin{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
         margin-bottom: 10%;
         width: 350px;
         padding: 4% 2%;
         border: 1px black solid;
         border-radius: 20px;
+    }
+    .block_task_begin_but{
+        border-radius: 20px;
+        background-color: brown;
+        color: aliceblue;
+        transition: all 0.2s;
+        
+    }
+    .block_task_begin_but:hover:active{
+        height: 30px;
+        width: 20%;
+        background-color: black;
+        color:#ffffff ;
+    }
+    .block_task_begin_but:hover{
+        height: 25px;
+        width: 18%;
+        background-color: #5b0000;
     }
     .block_task{
         display: flex;
@@ -65,14 +86,18 @@ export default {
         border-radius: 20px;
         transition: all 0.1s;
     }
-    .block_task_but:active{
+    .block_task_but:hover:active{
         height: 70px;
         width: 180px;
         border-radius: 30px;
         color: azure;
         font-size: 28px;
-        background-color: rgb(142, 143, 143);
-        
+        background-color: rgb(20, 5, 137);   
+    }
+    .block_task_but:hover{
+        height: 60px;
+        width: 170px;
+        background-color: #9f9d9d6b;
     }
     main{
         height: 40em;
